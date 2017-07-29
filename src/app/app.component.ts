5388d6ec73e767a6ad34761cdbbab19b3062c175
Victor de la Cruz <vcg_cruz@hotmail.com>
2017-07-28 19:31:05 -0500

    location management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {GoogleMapsService} from "google-maps-angular2";
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {isNullOrUndefined} from "util";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+
+  @ViewChild('mapElement') mapElement: ElementRef;
+  @ViewChild('inputElement') inputElement: ElementRef;
+  private map:any;
+  private lastMarker:any;
+  private points:FirebaseListObservable<any>;
+  constructor(private gapi:GoogleMapsService,private  database:AngularFireDatabase){
+    this.points = this.database.list("/locations");
+  }
+
+
+  ngAfterViewInit(){
+    /**
+     * Init map api [google.maps]
+     */
+    this.gapi.init.then(maps => {
+      const loc = new maps.LatLng(20.6737919, -103.3354132);
+
+      this.map = new maps.Map(this.mapElement.nativeElement, {
+        zoom: 13,
+        center: loc,
+        scrollwheel: true,
+        panControl: false,
+        mapTypeControl: false,
+        zoomControl: true,
+        streetViewControl: false,
+        scaleControl: true,
+        zoomControlOptions: {
+          style: maps.ZoomControlStyle.LARGE,
+          position: maps.ControlPosition.RIGHT_BOTTOM
+        }
+      });
+
+      /**
+       * Test gps
+       * */
+      this.points.subscribe(
+        next=>{
+          var flightPlanCoordinates:any = [];
+          let loc:any = new maps.LatLng(20.6737919, -103.3354132);
+          next.forEach(position=>{
+            flightPlanCoordinates.push({lat:position.lat,lng:position.lng})
+            loc = new maps.LatLng(position.lat, position.lng);
+          });
+
+          if(!isNullOrUndefined(this.lastMarker))
+            this.lastMarker.setMap(null);
+
+          this.lastMarker = new maps.Marker(
+            {
+              map: this.map,
+              position: loc,
+              zoom: 18,
+              draggable:false
+            }
+          );
+
+          var flightPath = new maps.Polyline({
+            path: flightPlanCoordinates,
+            geodesic: true,
+            strokeColor: '#34a4ff',
+            strokeOpacity: 1.0,
+            strokeWeight: 4
+          });
+
+          flightPath.setMap(this.map);
+          this.map.setZoom(18);
+          this.map.setCenter(loc);
+
+        }
+      );
+    });
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "angularfire2";
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {GoogleMapsModule} from "google-maps-angular2";
+
+export const config:any = {
+  apiKey: "AIzaSyDo--JtQxmFRFm32PMny3wP5z-Q8qIXm2g",
+  authDomain: "gps-field-services.firebaseapp.com",
+  databaseURL: "https://gps-field-services.firebaseio.com",
+  projectId: "gps-field-services",
+  storageBucket: "gps-field-services.appspot.com",
+  messagingSenderId: "209685429597"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config,"gps-field-services"),
+    AngularFireDatabaseModule,
+    GoogleMapsModule.forRoot({
+      url: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAQVcqyAcEuQrGITG_O8t5s_270UM3hmcc&callback=__onGoogleLoade'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
